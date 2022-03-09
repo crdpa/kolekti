df5ce6f0894cdc8751b347fcab5601d79fd4f555
Cristhyano <crisdpa@tuta.io>
2022-03-09 07:50:55 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<div align="center">
+  <h1 align="center">KOLEKTI</h1>
+
+  <p align="center">
+    Your own music listening database!<br><br>
+    <img alt="GitHub contributors" src="https://img.shields.io/github/contributors/crdpa/kolekti?style=for-the-badge">
+    <img alt="GitHub stars" src="https://img.shields.io/github/stars/crdpa/kolekti?style=for-the-badge">
+    <img alt="GitHub issues" src="https://img.shields.io/github/issues/crdpa/kolekti?style=for-the-badge"><br>
+  </p>
+</div>
+
+## ABOUT
+
+### Work in progress! Not usable yet.
+
+Kolekti displays info about [Musyca's database](https://github.com/crdpa/musyca/) in the terminal.
+
+## TODO
+
+[✓] Show top artists, top songs and top albums
+[ ] Implement start date and end date to retrieve data
+[ ] Design the UI
---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/charmbracelet/glamour"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var (
+	artistName string
+	albumName  string
+	songName   string
+	plays      string
+)
+
+func topSongs(database *sql.DB) string {
+	rows, err := database.Query(`SELECT artists.name, songs.name, COUNT(*) as count FROM songs LEFT JOIN albums ON songs.album = albums.id LEFT JOIN artists ON albums.artist = artists.id WHERE date>=date('2022-01-01') GROUP BY songs.name ORDER BY count DESC`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	table := fmt.Sprintf("| Artist | Song | # |\n| --- | --- | --- |\n")
+	for i := 1; rows.Next(); i++ {
+		rows.Scan(&artistName, &songName, &plays)
+		table += fmt.Sprintf("| %s | %s | %s |\n", artistName, songName, plays)
+		if i == 10 {
+			break
+		}
+	}
+	rows.Close()
+	return table
+}
+
+func topArtists(database *sql.DB) string {
+	rows, err := database.Query(`SELECT artists.name, COUNT(*) as count FROM songs LEFT JOIN albums ON songs.album = albums.id LEFT JOIN artists ON albums.artist = artists.id WHERE date>=date('2022-01-01') GROUP BY artists.name ORDER BY count DESC`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	table := fmt.Sprintf("| Artist | # |\n| --- | --- |\n")
+	for i := 1; rows.Next(); i++ {
+		rows.Scan(&artistName, &plays)
+		table += fmt.Sprintf("| %s | %s |\n", artistName, plays)
+		if i == 10 {
+			break
+		}
+	}
+	rows.Close()
+	return table
+}
+
+func topAlbums(database *sql.DB) string {
+	rows, err := database.Query(`SELECT albums.name, artists.name, COUNT(*) as count FROM songs LEFT JOIN albums ON songs.album = albums.id LEFT JOIN artists ON albums.artist = artists.id WHERE date>=date('2022-01-01') GROUP BY albums.id ORDER BY count DESC
+`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	table := fmt.Sprintf("| Album | Artist | # |\n| --- | --- | --- |\n")
+	for i := 1; rows.Next(); i++ {
+		rows.Scan(&albumName, &artistName, &plays)
+		table += fmt.Sprintf("| %s | %s | %s |\n", albumName, artistName, plays)
+		if i == 10 {
+			break
+		}
+	}
+	rows.Close()
+	return table
+}
+
+func main() {
+	var (
+		dbPath string
+		table  string
+		out    string
+	)
+	if len(os.Args) > 1 {
+		dbPath = os.Args[1]
+	} else {
+		dirname, err := os.UserConfigDir()
+		if err != nil {
+			log.Fatal(err)
+		}
+		dbPath = dirname + "/musyca/database.db"
+	}
+
+	database, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	table = topArtists(database)
+	out, err = glamour.Render(table, "dark")
+	fmt.Print(out)
+	database.Close()
+}
